2a7ed49ed858e1e0a14aea9f71b9f75df7eba0bc
Gabrieljll <48096093+Gabrieljll@users.noreply.github.com>
2023-12-24 11:34:01 -0300

    Fix Navigate in privateRoutes

--- src/router/PrivateRoutes.js
+++ src/router/PrivateRoutes.js
@@ -3,12 +3,13 @@ import Cart from '../components/Cart/Cart';
 import { Navbar } from '../components/NavBar/NavBar';
 import {ItemListContainer} from '../components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from '../components/ItemDetailContainer/ItemDetailContainer';
-import { Routes, Route } from "react-router-dom";
 import { Checkout } from '../components/Checkout/Checkout';
 import { HomeScreen } from '../components/HomeScreen/HomeScreen';
 import { ErrorScreen } from '../components/ErrorScreen/ErrorScreen';
 import { Footer } from '../components/Footer/Footer';
 import { FormularioMail } from '../components/FormularioMail/FormularioMail';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 export const PrivateRoutes = () => {
     return (
         <div className="App">
@@ -21,7 +22,7 @@ export const PrivateRoutes = () => {
                 <Route path="/detail/:itemId" element={ <ItemDetailContainer /> }/>
                 <Route path="/cart" element={ <Cart /> }/>
                 <Route path="/checkout" element={ <Checkout /> }/>
-                <Route path="/*" element={ <HomeScreen/> }/>
+                <Route path="*" element={<Navigate to="/home" replace />}/>
             </Routes>
             <Footer />
         </div>
