49475c68d2482a2f9858f1eefad2587b55d219dd
Gabrieljll <48096093+Gabrieljll@users.noreply.github.com>
2023-12-24 02:18:35 -0300

    Redirect home

--- src/router/PublicRoutes.js
+++ src/router/PublicRoutes.js
@@ -7,7 +7,7 @@ export const PublicRoutes = () => {
         <Routes>
             <Route path="/login" element={ <LoginScreen /> }/>
             <Route path="/register" element={ <RegisterScreen /> }/>
-            <Route path="/*" element={ <ErrorScreen error="Debe loguearse primero" logged={false}/> }/>
+            <Route path="/*" element={ <ErrorScreen error="Debe loguearse primero" logged={true}/> }/>
         </Routes>
     )
 }
