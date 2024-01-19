471a38b3c0ff608819d7db15a7c52d79557a205c
Gabrieljll <48096093+Gabrieljll@users.noreply.github.com>
2024-01-19 13:35:39 -0300

    CartView

--- src/components/CartView/CartView.js
+++ src/components/CartView/CartView.js
@@ -51,21 +51,23 @@ const CartView = () => {
                     cart.map(item => (
                         <div className="flex justify-center items-center gap-y-2 lg:h-[150px] h-[200px] px-5" key={item.id}>
                             <div className="flex items-center gap-x-16 py-12 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
-                                <div className="w-full max-h-[100px] flex justify-center items-center gap-x-4">
+                                <div className="w-full max-h-[100px] flex justify-center items-center gap-x-4 ">
                                     <div className="max-w-[80px] max-h-[150px] h-[100px]">
                                         <img className="self-start" src={item.image} alt="" />
                                     </div>
-                                    <div className="text-sm md:text-lg w-64 max-w-[350px] font-bold gap-x-4">
+                                    <div className="text-sm md:text-lg w-36 max-w-[350px] font-bold gap-x-4">
                                         <h3>{item.title}</h3>                                
                                     </div>
                                     <div>
-                                        <h5 className="text-sm lg:text-2xl">Precio/u: <span className="lg:text-xl font-bold">${item.price * item.amount}</span></h5>
-                                    </div>
-                                    <div className="">
-                                        <h5 className="text-sm lg:text-2xl">Cantidad: <span className="lg:text-xl font-bold">{item.amount}</span></h5>
-                                    </div>
-                                    <div className="">
-                                        <h5 className="text-sm lg:text-2xl">Total de producto: <span className="lg:text-xl font-bold">${`${parseFloat(item.price*item.amount).toFixed(2)}`}</span> </h5>
+                                        <div>
+                                            <h5 className="text-sm lg:text-2xl">Precio/u: <span className="lg:text-xl font-bold">${item.price * item.amount}</span></h5>
+                                        </div>
+                                        <div className="">
+                                            <h5 className="text-sm lg:text-2xl">Cantidad: <span className="lg:text-xl font-bold">{item.amount}</span></h5>
+                                        </div>
+                                        <div className="">
+                                            <h5 className="text-sm lg:text-2xl">Total de producto: <span className="lg:text-xl font-bold">${`${parseFloat(item.price*item.amount).toFixed(2)}`}</span> </h5>
+                                        </div>
                                     </div>
                                      {/* remove icon */}
                                     <div onClick={() => removeFromCart(item.id)} className="lg:text-xl cursor-pointer">
