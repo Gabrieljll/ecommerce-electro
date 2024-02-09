1999829fbef5c17d8e8332106431e1d2c939c6ec
Gabrieljll <48096093+Gabrieljll@users.noreply.github.com>
2024-02-09 15:14:46 -0300

    Fix handleClose SideBar

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -4,10 +4,11 @@ import {Link} from 'react-router-dom'
 import { IoMdAdd, IoMdClose, IoMdRemove} from 'react-icons/io'
 // import cart context
 import {CartContext} from '../../context/CartContext'
-
+import {SidebarContext} from '../../context/SidebarContext'
 
 export const CartItem = ({item}) => {
     const {removeFromCart, increaseAmount, decreaseAmount} = useContext(CartContext)
+    const { handleClose } = useContext(SidebarContext)
     // destrictire ote,
     const {id, title, image, price, amount} = item;
     
@@ -15,7 +16,7 @@ export const CartItem = ({item}) => {
         <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
             <div className="w-full min-h-[150px] flex items-center gap-x-4">
                 {/* image */}
-                <Link to={`/product/${id}`}>
+                <Link onClick={handleClose} to={`/product/${id}`}>
                     <img className="max-w-[80px]" src={image} alt={title} />
                 </Link>
                 <div className="w-full flex flex-col">
