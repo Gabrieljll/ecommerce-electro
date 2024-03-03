c736cccf1c9cf8b44e52d8dee26d304e3be52106
Gabrieljll <48096093+Gabrieljll@users.noreply.github.com>
2024-03-03 15:27:26 -0300

    console

--- src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -3,6 +3,7 @@ import {Navigate, Outlet} from 'react-router-dom'
 export const ProtectedRoutes = ({ user, children }) => {
     
     if(!user){
+        console.log("falla en protectedRoutes")
         return <Navigate to="/login" />
     }
     return <Outlet/>
