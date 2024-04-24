5e3fa0780c59c421fe158c90dc24c92e88874ed4
Gabrieljll <48096093+Gabrieljll@users.noreply.github.com>
2024-04-24 17:14:03 -0300

    Fix home images

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -16,14 +16,14 @@ const image4_movil = "/images/nuevas_imgs/sevicio_tecnico_cuadrado.png"
 const image5_movil = "/images/nuevas_imgs/atencion_personalizada_cuadrado.png"
 
 export const Slider = () => {
-  const isMobile = window.innerWidth < 1000;
+  const isMobile = window.innerWidth < 1024;
   const slidesPC = [
     { url: image1 },
     { url: image2 },
     { url: image3 },
     { url: image4 },
     { url: image5 }
-    
+
   ];
 
   const slidesMovil = [
@@ -81,52 +81,63 @@ export const Slider = () => {
     }, 100);
   }, []);
 
-  return (
-    <div className={`max-w-[1920px] ${isMobile ? 'h-[100vw]' : 'h-[950px]'} w-full m-auto pb-5 relative group transition-fade-up ${isActive ? 'active' : ''}`}>
-      <div
-        style={{
-          backgroundImage: `url(${slides[currentIndex].url})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          height: isMobile ? '100vw' : '950px', // Altura específica para las imágenes de fondo
-          transition: 'background-image 1s ease', // Solo transición de la imagen de fondo
-        }}
-        className="w-full h-full object-cover duration-500"
-      >
-        {isMobile && (
-          <img
-            src={slides[currentIndex].url}
-            alt={`Slide ${currentIndex + 1}`}
-            className="w-full h-full object-fit"
-            style={{
-                width: '100%', // Ajuste para dispositivos móviles
-                height: '100%', // Esto asegura que la imagen tenga la altura completa en dispositivos móviles
-                opacity: isActive ? 1 : 0,
-                transition: 'opacity 0.1s ease'
-            }}
-          />
-        )}
-      </div>
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlide} size={30} />
-      </div>
-      <div
-        ref={chevronRightRef}
-        className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactRight onClick={nextSlide} size={30} />
-      </div>
-      <div className='flex top-4 justify-center py-2'>
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
-          >
-            <RxDotFilled />
-          </div>
-        ))}
-      </div>
-    </div>
+  return ( <
+    div className = { `max-w-[1920px] ${isMobile ? 'h-[100vw]' : 'h-[950px]'} w-full m-auto pb-5 relative group transition-fade-up ${isActive ? 'active' : ''}` } >
+    <
+    div style = {
+      {
+        backgroundImage: `url(${slides[currentIndex].url})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        height: isMobile ? '100vw' : '950px', // Altura específica para las imágenes de fondo
+        transition: 'background-image 1s ease', // Solo transición de la imagen de fondo
+      }
+    }
+    className = "w-full h-full object-cover duration-500" > {
+      isMobile && ( <
+        img src = { slides[currentIndex].url }
+        alt = { `Slide ${currentIndex + 1}` }
+        className = "w-full h-full object-fit"
+        style = {
+          {
+            width: '100%', // Ajuste para dispositivos móviles
+            height: '100%', // Esto asegura que la imagen tenga la altura completa en dispositivos móviles
+            opacity: isActive ? 1 : 0,
+            transition: 'opacity 0.1s ease'
+          }
+        }
+        />
+      )
+    } <
+    /div> <
+    div className = 'hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer' >
+    <
+    BsChevronCompactLeft onClick = { prevSlide }
+    size = { 30 }
+    /> < /
+    div > <
+    div ref = { chevronRightRef }
+    className = 'hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer' >
+    <
+    BsChevronCompactRight onClick = { nextSlide }
+    size = { 30 }
+    /> < /
+    div > <
+    div className = 'flex top-4 justify-center py-2' > {
+      slides.map((slide, slideIndex) => ( <
+        div key = { slideIndex }
+        onClick = {
+          () => goToSlide(slideIndex)
+        }
+        className = 'text-2xl cursor-pointer' >
+        <
+        RxDotFilled / >
+        <
+        /div>
+      ))
+    } <
+    /div> < /
+    div >
   );
 }
