dd31e611a83014302feb841d9c29685653b9b52b
Gabrieljll <48096093+Gabrieljll@users.noreply.github.com>
2024-05-09 22:26:23 -0300

    Fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -34,16 +34,25 @@ export const Footer = () => {
         }, 0);
     };
     return (
-    <footer className="bg-black py-5">
+    <footer className="bg-slate-950 border-t-0 py-5">
     <section className="wrapper grid gap-12 justify-center items-center footer-area md:footer-area-md md:grid-cols-3 md:justify-items-center">
 
-        <nav className="grid gap-y-4 h-min justify-center text-center text-white w-full md:w-full md:grid-cols-2 lg:grid-cols-3">
-            <Link to="/home" title="Home | CJRepuestos" onClick={scrollToTop}> <h1 className="text-md font-[ArchivoBlack-Regular] text-white ">Inicio</h1></Link>
-            <Link to="/productos" title="Productos | CJRepuestos" onClick={scrollToTop}><h1 className="text-md font-[ArchivoBlack-Regular] text-white ">Tienda</h1></Link>
-{/*             <LinkScroll onClick={() => redirectToHomeAndScroll("nosotros")} duration={500}><h1 className="text-md font-[ArchivoBlack-Regular] text-white">Sobre Nosotros</h1></LinkScroll>
+        <nav className="grid gap-y-4 h-min justify-center text-center text-white w-full md:w-max md:grid-cols-2 lg:grid-cols-3">
+            <Link to="/home" title="Home | CJRepuestos" onClick={scrollToTop}> <h1 className="text-md font-[ArchivoBlack-Regular] text-slate-50 ">Inicio</h1></Link>
+            <Link to="/productos" title="Productos | CJRepuestos" onClick={scrollToTop}><h1 className="text-md font-[ArchivoBlack-Regular] text-slate-50 ">Tienda</h1></Link>
+            <div className="text-md font-[ArchivoBlack-Regular] text-white">
+                <h1 className="text-md font-[ArchivoBlack-Regular] text-white">Horario de atención:</h1>
+                <p className="font-[Arimo-Regular] text-sm">Lunes a Viernes de 9 a 18hs</p>
+                <p className="font-[Arimo-Regular] text-sm">Sábados de 9 a 12hs*</p>
+            
+            </div>
             <Link to="/contactenos"><h1 className="text-md font-[ArchivoBlack-Regular] text-white">Contacto</h1></Link>
-            <LinkScroll onClick={() => redirectToHomeAndScroll("atencionCliente")} duration={500}><h1 className="text-md font-[ArchivoBlack-Regular] text-white">Atención al Cliente</h1></LinkScroll>
-            <Link to={urlWsp} target="_blank"><h1 className="text-md font-[ArchivoBlack-Regular] text-white">WhatsApp</h1></Link> */}
+            <div>
+                <h1 className="text-md font-[ArchivoBlack-Regular] text-white">Atención al Cliente:</h1>
+                <p className="font-[Arimo-Regular] text-sm"> +54 1134890403</p>
+                <p className="font-[Arimo-Regular] text-sm">jcejas.repuestos@gmail.com</p>                
+            </div>
+            <Link to={urlWsp} target="_blank"><h1 className="text-md font-[ArchivoBlack-Regular] text-white">WhatsApp</h1></Link>
         </nav>
 
         <div className="flex flex-wrap gap-4 justify-center w-full md:justify-center">
@@ -63,8 +72,8 @@ export const Footer = () => {
             <img className="w-[100%]" src={logoImg} alt="Logo cjrepuestos Footer" title='Logo CJRepuestos Nav'/>
         </Link>
 
-        <p className="text-dark-grayish-blue text-center md:text-right md:col-span-2 lg:col-span-3">
-        Copyright 2023. All Rights Reserved
+        <p className="text-slate-500 text-center md:text-right md:col-span-2 lg:col-span-3">
+        Diseñado por <Link className="text-slate-400 underline" to="https://www.linkedin.com/in/gabriel-leguizam%C3%B3n-910bb0182/" title="Gabriel Leguizamón">Gabriel Leguizamón | Desarrollador Web.</Link> Todos los derechos reservados 2024.
         </p>
     </section>
     </footer>
